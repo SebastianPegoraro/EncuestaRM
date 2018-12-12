5bd5c1a64799d4525a738588c6a22672a97af950
SebastianPegoraro <pegorarosebastian@gmail.com>
2018-12-12 13:17:47 -0300

    Intento de que funcione agregar opciones... ya no realiza el submit.

--- admin/nuevaEncuesta.php
+++ admin/nuevaEncuesta.php
@@ -20,10 +20,12 @@ $tipos = Tipo::showTipos();
                         </div>
                     </div>
                     <div class="card cuadro" id="pregunta">
-                        <div class="card-body">
+                        <div class="card-header">
                             <div class="form-group col-sm-12">
-                                <h4>Pregunta</h4>
+                                <h4>Preguntas</h4>
                             </div>
+                        </div> 
+                        <div class="card-body">                                                       
                             <!-- Empieza bloque de preguntas -->
                             <div class='row rowPregunta'>
                                 <div class='form-group col-sm-12'>
@@ -31,7 +33,7 @@ $tipos = Tipo::showTipos();
                                 </div>
                                 <!-- Empieza bloque de opciones -->
                                 <div class='container'>
-                                    <div class='row'>
+                                    <div class='row rowOpcion'>
                                         <div class='form-group col-sm-3 col-12'>
                                             <select class='custom-select' name='tipo[]' required>
                                                 <option selected disabled>Tipo de Opción</option>
@@ -45,7 +47,7 @@ $tipos = Tipo::showTipos();
                                         </div>                                         
                                     </div>
                                     <div class='col-12 text-right'>
-                                        <button class='btn btn-outline-success boton' onclick='add_row_opcion();'> Agregar otra opción </button>
+                                        <input type='button' value='Agregar otra Opcion' class='btn btn-outline-success' onclick='add_row_opcion()'>
                                     </div>
                                     <br>
                                 </div>
@@ -53,11 +55,16 @@ $tipos = Tipo::showTipos();
                             </div>
                             <!-- Termina bloque de preguntas -->
                         </div>
+                        <div class="card-footer">
+                            <div class='col-12 text-center'>
+                                <button class='btn btn-outline-default' type="submit"> Crear Encuesta </button>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
             <div class='col-12 text-right'>
-                <button class='btn btn-outline-success boton' onclick='add_row_pregunta();'> Agregar otra pregunta </button>
+                <input value=' Agregar otra pregunta ' class='btn btn-outline-success' onclick='add_row_pregunta()'>
             </div>  
         </div>
     </div>

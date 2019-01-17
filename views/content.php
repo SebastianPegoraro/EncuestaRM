aeb1707dc0b1528c7b7018f3b0e28f398dda0dfe
SebastianPegoraro <pegorarosebastian@gmail.com>
2019-01-17 15:34:27 -0300

    Cambios para que funcionen los checkbox, manteniendo el diseño

--- views/content.php
+++ views/content.php
@@ -54,30 +54,26 @@ $cont = 0;
                         $contOpcion++
                         ?>
                         <div class="col-12 text-center">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="form-group col text-center">
-                                        <?php if($tipo->getClase() == 'radio'){
-                                            ?>
-                                            <input class="form-check-input" type='<?php echo $tipo->getClase() ?>' name='opcion<?php echo $cont ?>' value='<?php echo $eleccion->getDescripcion() ?>' required>
-                                            <label><?php echo $eleccion->getDescripcion() ?></label>
-                                            <?php
-                                        } else if ($tipo->getClase() == 'checkbox') {
-                                            ?>
-                                            <div class = "col-2 col-sm-2 text-left">
-                                              <input class="form-check-input" type='<?php echo $tipo->getClase() ?>' name='opcion<?php echo $cont ?><?php echo $contOpcion ?>' value='<?php echo $eleccion->getDescripcion() ?>'>
-                                            </div>
-                                            <div class="col-12 col-sm-6 text-left" style="padding-left:50px;">
-                                              <label><?php echo $eleccion->getDescripcion() ?></label>
-                                            </div>
-                                            <?php
-                                        } else if ($tipo->getClase() == 'text') {
-                                            ?>
-                                            <input class="form-control" type='<?php echo $tipo->getClase() ?>' name='opcion<?php echo $cont ?>' placeholder="<?php echo $eleccion->getDescripcion() ?>" required>
-                                            <?php
-                                        }
+                            <div class="row">
+                                <div class="form-group col text-center">
+                                    <?php if($tipo->getClase() == 'radio'){
                                         ?>
-                                    </div>
+                                        <input class="form-check-input" type='<?php echo $tipo->getClase() ?>' name='opcion<?php echo $cont ?>' value='<?php echo $eleccion->getDescripcion() ?>' required>
+                                        <label><?php echo $eleccion->getDescripcion() ?></label>
+                                        <?php
+                                    } else if ($tipo->getClase() == 'checkbox') {
+                                        ?>
+                                        <div class="col-8 offset-2 text-left">
+                                            <input class="form-check-input" type='<?php echo $tipo->getClase() ?>' name='opcion<?php echo $cont ?><?php echo $contOpcion ?>' value='<?php echo $eleccion->getDescripcion() ?>'>
+                                            <label><?php echo $eleccion->getDescripcion() ?></label>
+                                        </div>                                        
+                                        <?php
+                                    } else if ($tipo->getClase() == 'text') {
+                                        ?>
+                                        <input class="form-control" type='<?php echo $tipo->getClase() ?>' name='opcion<?php echo $cont ?>' placeholder="<?php echo $eleccion->getDescripcion() ?>" required>
+                                        <?php
+                                    }
+                                    ?>
                                 </div>
                             </div>
                         </div>
